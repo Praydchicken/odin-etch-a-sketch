95a1d24e16843c0def2639792bd62986b65724d2
Mark <111501559+Praydchicken@users.noreply.github.com>
2026-01-01 22:23:21 +0800

    feat: add createGrid function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const boardElement = document.querySelector('.js-board');
+
+function createGrid(gridSize = 10) {
+	for (let i = 0; i < gridSize; i++) {
+		const gridElement = document.createElement('div');
+		gridElement.classList.add('row');
+		boardElement.appendChild(gridElement);
+
+		for (let j = 0; j < gridSize; j++) {
+			const pixelElement = document.createElement('div');
+			pixelElement.classList.add('row__pixel');
+			gridElement.appendChild(pixelElement);
+		}
+	}
+}
