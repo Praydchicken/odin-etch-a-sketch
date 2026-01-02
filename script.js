6dd9396fe1e68dd97a35ff717b92309b214086fb
Mark <111501559+Praydchicken@users.noreply.github.com>
2026-01-02 13:56:01 +0800

    feat: add progressive and rainbow drawing mode -create a new function draw that facilitates how it will render pixels -add buttons that toggles progressive and rainbow drawing mode

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const boardElement = document.querySelector('.js-board');
 const inputElement = document.querySelector('.js-input-size');
 const buttonElement = document.querySelector('.js-btn-size');
+const drawControlElement = document.querySelector('.js-draw-controls');
+
+const settings = {
+	'progressive': false,
+	'rainbow': false,
+	'opacity': 0.1
+};
 
 boardElement.addEventListener('mouseover', event => {
 	if(event.target.classList.contains('row__pixel')) {
-		event.target.classList.add('row__pixel--draw');
+		draw(event.target);
 	}
 });
 
@@ -23,6 +30,20 @@ buttonElement.addEventListener('click', (event) => {
 	inputElement.value = '';
 
 	event.preventDefault();
+});
+
+drawControlElement.addEventListener('click', (event) => {
+	const btn = event.target;
+
+	if(btn.classList.contains('js-progressive')) {
+		settings.progressive = !settings.progressive;
+		btn.textContent = settings.progressive ? 'PROGRESSIVE MODE: ON' : 'PROGRESSIVE MODE: OFF';
+	}
+
+	if(btn.classList.contains('js-rainbow')) {
+		settings.rainbow = !settings.rainbow;
+		btn.textContent = settings.rainbow ? 'RAINBOW MODE: ON' : 'RAINBOW MODE: OFF';
+	}
 });
 
 function createGrid(gridSize = 10) {
@@ -49,6 +70,30 @@ function changeGridSize(size) {
 	}
 
 	createGrid(size);
+}
+
+function draw(pixel) {
+	pixel.classList.add('row__pixel--draw');
+
+	if(pixel.style.opacity === '1') {
+		return;
+	}
+
+	if(settings.progressive) {
+		const currentOpacity = parseFloat(pixel.style.opacity || 0);
+		pixel.style.opacity = Math.min(currentOpacity + settings.opacity, 1);
+	} else {
+		pixel.style.opacity = 1;
+	}
+
+	if(settings.rainbow) {
+		const red = Math.floor(Math.random() * 256);
+		const green = Math.floor(Math.random() * 256);
+		const blue = Math.floor(Math.random() * 256);
+
+		pixel.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+			console.log('Color set to:', pixel.style.backgroundColor);
+	}
 }
 
 createGrid();
